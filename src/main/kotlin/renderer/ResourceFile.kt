454b8a56b39cdc7621d53af3d93351388b4091a4
leo_songwei <leo_songwei@outlook.com>
2022-03-31 20:08:21 +1100

    resource file

---
+++ src/main/kotlin/renderer/ResourceFile.kt
@@ -0,0 +1,11 @@
+class ResourceFile {
+    fun getAsBytes(filepath: String?): ByteArray? {
+        return try {
+            val classLoader = javaClass.classLoader
+            val stream = classLoader.getResourceAsStream(filepath)!!
+            stream.readAllBytes()
+        } catch (e: Exception) {
+            null
+        }
+    }
+}
