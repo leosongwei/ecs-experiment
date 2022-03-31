752632c35960432728644b96f026aa05f9915eaf
leo_songwei <leo_songwei@outlook.com>
2022-03-31 20:12:20 +1100

    revert

--- src/main/kotlin/renderer/ResourceFile.kt
+++ src/main/kotlin/renderer/ResourceFile.kt
@@ -1,15 +1,13 @@
 package renderer
 
 class ResourceFile {
-    companion object {
-        fun getAsBytes(filepath: String?): ByteArray? {
-            return try {
-                val classLoader = javaClass.classLoader
-                val stream = classLoader.getResourceAsStream(filepath)!!
-                stream.readAllBytes()
-            } catch (e: Exception) {
-                null
-            }
+    fun getAsBytes(filepath: String?): ByteArray? {
+        return try {
+            val classLoader = javaClass.classLoader
+            val stream = classLoader.getResourceAsStream(filepath)!!
+            stream.readAllBytes()
+        } catch (e: Exception) {
+            null
         }
     }
 }
