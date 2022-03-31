adc8ef82d54b470b07e143a2cbff492d0ace302b
leo_songwei <leo_songwei@outlook.com>
2022-03-31 20:11:47 +1100

    companion

--- src/main/kotlin/renderer/ResourceFile.kt
+++ src/main/kotlin/renderer/ResourceFile.kt
@@ -1,11 +1,15 @@
+package renderer
+
 class ResourceFile {
-    fun getAsBytes(filepath: String?): ByteArray? {
-        return try {
-            val classLoader = javaClass.classLoader
-            val stream = classLoader.getResourceAsStream(filepath)!!
-            stream.readAllBytes()
-        } catch (e: Exception) {
-            null
+    companion object {
+        fun getAsBytes(filepath: String?): ByteArray? {
+            return try {
+                val classLoader = javaClass.classLoader
+                val stream = classLoader.getResourceAsStream(filepath)!!
+                stream.readAllBytes()
+            } catch (e: Exception) {
+                null
+            }
         }
     }
 }
